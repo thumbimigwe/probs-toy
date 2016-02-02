3ff8dbdf44d618fdbf32f9c1f0ee2b3db01d2f1a
thumbimigwe <thumbimigwe@gmail.com>
2016-02-02 10:08:43 +0300

    save

---
+++ Toy Problems/src/feb_1st/MonthNames.java
@@ -0,0 +1,54 @@
+//Using Java, declare an int named month whose value represents a month. 
+//Your code should displays the name of the month, based on the value of month,
+//using the switch statement.
+
+import java.util.Scanner;
+
+package feb_1st;
+
+public class MonthNames {
+	public static void main(String[] args) {
+		int month = int Math.random() *20);
+		String month_name;
+		
+		switch		
+		case 1: month_name="January";
+		break
+		
+		case 1: month_name="February";
+		break
+		
+		case 1: month_name="March";
+		break
+		
+		case 1: month_name="April";
+		break
+		
+		case 1: month_name="May";
+		break
+		
+		case 1: month_name="June";
+		break
+		
+		case 1: month_name="July";
+		break
+		
+		case 1: month_name="August";
+		break
+		
+		case 1: month_name="September";
+		break
+		
+		case 1: month_name="October";
+		break
+		
+		case 1: month_name="November";
+		break
+		
+		case 1: month_name="December";
+		break
+		
+
+	}
+
+}
