b2878d9ac990e0bc17c1fdeb9d9216ecf789a4e7
thumbimigwe <thumbimigwe@gmail.com>
2016-02-03 11:43:27 +0300

    2nd feb toy problem

---
+++ Toy Problems/src/feb_2nd/VowelCount.java
@@ -0,0 +1,38 @@
+package feb_2nd;
+
+//PSEUDOCODE
+//* Created one class with main method  (Main + VowelCount)
+//* created a print messege for user 		(in Main)
+//	-reads reply
+//
+//* Call vowel count using string as a parameter	(in Vowel count)
+//* Make string in lower case
+//* Loop through each character
+//* Use condition to check if it is a  vowel
+//* Count vowels
+// 
+//* Create object of HashMap
+//* Added values to HashMap
+//* Return Hashmap
+//
+//* Create a loop through all values and keys
+//* Print them
+//
+
+
+
+
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+
+
+public class VowelCount {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
