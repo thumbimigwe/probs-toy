32a26204d537a9509d4d19e599c894803292e8d2
thumbimigwe <thumbimigwe@gmail.com>
2016-02-10 12:25:50 +0300

    Highest Number

---
+++ Toy Problems/src/feb_10th/nextHighestNumber.java
@@ -0,0 +1,24 @@
+package feb_10th;
+
+import java.util.Arrays;
+
+//	########## PSEUDOCODE ########## 
+//	Declare and initialize an integer
+//	convert integer to an array of characters
+//	sort the array
+//	loop through the array in reverse order.
+
+
+
+public class nextHighestNumber {
+
+	public static void main(String[] args) {
+		int number = 1508;
+		char[] x = String.valueOf(number).toCharArray();
+		Arrays.sort(x);
+		
+		for (int i = x.length - 1;	i>=0;	i--){
+			System.out.print(x[i]);
+		}
+	}	
+}
