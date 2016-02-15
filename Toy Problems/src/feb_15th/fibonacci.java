2a215f3d6bdd6a8b009ef69fdc585067789435ea
thumbimigwe <thumbimigwe@gmail.com>
2016-02-15 13:01:44 +0300

    fibonacci soln 3

--- Toy Problems/src/feb_15th/fibonacci.java
+++ Toy Problems/src/feb_15th/fibonacci.java
@@ -2,38 +2,38 @@ package feb_15th;
 
 import java.util.Scanner;
 
-//***********SOLUTION # 1 - for all the fibonacci***********
-public class fibonacci
-{
-	public static void main(String[] args)
-	{
-	       Scanner scan = new Scanner(System.in);
-	       System.out.println("Enter the value of n: ");
-	       int n = scan.nextInt();
-	       for (int i = 0; i <= n; i++) 
-	       {
-	           System.out.print(fibonacci(i) + " ");
-	       }
-	   }
-
-	   public static int fibonacci(int n)
-	   {
-	       if (n == 0)
-	       {
-	           return 0;
-	       }
-	       
-	       else if (n == 1)
-	       {
-	           return 1;
-	       }
-	       
-	       else
-	       {
-	           return fibonacci(n - 1) + fibonacci(n - 2);
-	       }
-	   }	   
-}
+//	//***********SOLUTION # 1 - for all the fibonacci***********
+//	public class fibonacci
+//	{
+//		public static void main(String[] args)
+//		{
+//		       Scanner scan = new Scanner(System.in);
+//		       System.out.println("Enter the value of n: ");
+//		       int n = scan.nextInt();
+//		       for (int i = 0; i <= n; i++) 
+//		       {
+//		           System.out.print(fibonacci(i) + " ");
+//		       }
+//		   }
+//	
+//		   public static int fibonacci(int n)
+//		   {
+//		       if (n == 0)
+//		       {
+//		           return 0;
+//		       }
+//		       
+//		       else if (n == 1)
+//		       {
+//		           return 1;
+//		       }
+//		       
+//		       else
+//		       {
+//		           return fibonacci(n - 1) + fibonacci(n - 2);
+//		       }
+//		   }	   
+//	}
 
 //***********SOLUTION # 2 - for all the fibonacci***********
 //
@@ -58,3 +58,42 @@ public class fibonacci
 //		return fibTerm;
 //	}
 //}
+
+
+public class Fibonacci {
+	
+	public static int fibonacciSequence(int number)
+	{
+		if(number == 0){
+            return 0;
+        }
+		else if(number == 1 || number == 2){
+            return 1;
+        }
+ 
+        return fibonacciSequence(number-1) + fibonacciSequence(number -2); //tail recursion
+	}
+  
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the position of the number in the fibonnaci sequence: ");
+		int num = input.nextInt();
+		int position = num-1;
+		System.out.println(fibonacciSequence(position));
+		
+		System.out.println("\n\nFibonacci series upto " + num +" numbers : ");
+        //printing Fibonacci series upto number
+        for(int i=0; i<=position; i++){
+            System.out.print(fibonacciSequence(i) +" ");
+        }
+
+	}
+
+}
+
+
+//***********SOLUTION # 3 - the fibonacci***********
