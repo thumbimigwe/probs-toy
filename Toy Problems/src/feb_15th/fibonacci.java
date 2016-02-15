2ddbb27e31a48a543bc037498870b9d6f6622e3e
thumbimigwe <thumbimigwe@gmail.com>
2016-02-15 12:59:14 +0300

    fibonacci soln 2

--- Toy Problems/src/feb_15th/fibonacci.java
+++ Toy Problems/src/feb_15th/fibonacci.java
@@ -34,3 +34,27 @@ public class fibonacci
 	       }
 	   }	   
 }
+
+//***********SOLUTION # 2 - for all the fibonacci***********
+//
+//public class fibonacci {
+//	
+//	public static void main(String[] args)
+//	{
+//		int index = 0;
+//		while (true)
+//		{
+//			System.out.println(fib(index));
+//			index++;
+//		}
+//	}
+//	
+//	public static long fib (int i)
+//	{
+//		if (i == 0) return 0;
+//		if (i <= 2) return 1;
+//		
+//		long fibTerm = fib(i -1) + (i - 2);
+//		return fibTerm;
+//	}
+//}
